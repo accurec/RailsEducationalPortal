96d05ebb7662259f0aae08c34678e3eceb8a1800
accurec <max.zhernovkov@gmail.com>
2025-08-10 17:45:26 +0000

    Refactor purcahse logic for term out of controller into helper

--- app/controllers/student/terms_to_purchase_controller.rb
+++ app/controllers/student/terms_to_purchase_controller.rb
@@ -1,4 +1,6 @@
 class Student::TermsToPurchaseController < ApplicationController
+  include Student::TermsToPurchaseHelper
+
   before_action :authenticate_user!
 
   def index
@@ -22,50 +24,17 @@ class Student::TermsToPurchaseController < ApplicationController
 
     payment_processor = ::PaymentProcessor.new(current_user)
     payment_result = payment_processor.process_term_payment(term)
-    payment_method = case payment_result[:payment_method]
-                     when 'credit_card'
-                       TermPurchase.payment_methods[:credit_card]
-                     when 'license_code'
-                       TermPurchase.payment_methods[:license_code]
-                     else
-                       TermPurchase.payment_methods[:other]
-                     end
 
     if payment_result[:success]
-      begin
-        ActiveRecord::Base.transaction do
-          term_purchase = current_user.term_purchases.build(
-            term: term, 
-            purchased_at: Time.current, 
-            payment_method: payment_method
-          )
-
-          term_purchase.save!
-
-          unpurchased_courses = term.courses.where.not(
-            id: current_user.purchased_courses.select(:id)
-          )
-          unpurchased_courses_count = unpurchased_courses.count
-          
-          unpurchased_courses.each do |course|
-            course_purchase = current_user.course_purchases.build(
-              course: course,
-              purchased_at: Time.current,
-              payment_method: payment_method
-            )
-
-            course_purchase.save!
-          end
-
-          flash[:notice] = "Successfully purchased #{term.name} for $#{payment_result[:amount]}! Added #{unpurchased_courses_count} new courses to your account."
-          redirect_to student_terms_to_purchase_path
-        end
-      rescue ActiveRecord::RecordInvalid => e
-        Rails.logger.error("Failed to purchase #{term.name}: #{e.message}")
-        
+      purchase_result = process_term_purchase(term, current_user, payment_result)
+      
+      if purchase_result[:success]
+        flash[:notice] = "Successfully purchased #{term.name} for $#{payment_result[:amount]}! Added #{purchase_result[:courses_added]} new courses to your account."
+      else
         flash[:alert] = "Failed to purchase #{term.name}. Please try again."
-        redirect_to student_terms_to_purchase_path
       end
+      
+      redirect_to student_terms_to_purchase_path
     else
       flash[:alert] = "Payment failed: #{payment_result[:error]}"
       redirect_to student_terms_to_purchase_path
--- app/helpers/student/terms_to_purchase_helper.rb
+++ app/helpers/student/terms_to_purchase_helper.rb
@@ -1,2 +1,54 @@
 module Student::TermsToPurchaseHelper
+  def process_term_purchase(term, user, payment_result)
+    payment_method = case payment_result[:payment_method]
+                      when 'credit_card'
+                        TermPurchase.payment_methods[:credit_card]
+                      when 'license_code'
+                        TermPurchase.payment_methods[:license_code]
+                      else
+                        TermPurchase.payment_methods[:other]
+                      end
+    
+    ActiveRecord::Base.transaction do
+      term_purchase = create_term_purchase(term, user, payment_method)
+      courses_added = create_course_purchases(term, user, payment_method)
+      
+      {
+        success: true,
+        term_purchase: term_purchase,
+        courses_added: courses_added
+      }
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    Rails.logger.error("Failed to purchase #{term.name}: #{e.message}")
+    { success: false, error: e.message }
+  end
+  
+  private
+  
+  def create_term_purchase(term, user, payment_method)
+    user.term_purchases.create!(
+      term: term,
+      purchased_at: Time.current,
+      payment_method: payment_method
+    )
+  end
+  
+  def create_course_purchases(term, user, payment_method)
+    unpurchased_courses = term.courses.where.not(
+      id: user.purchased_courses.select(:id)
+    )
+
+    unpurchased_courses_count = unpurchased_courses.count
+    
+    unpurchased_courses.each do |course|
+      user.course_purchases.create!(
+        course: course,
+        purchased_at: Time.current,
+        payment_method: payment_method
+      )
+    end
+    
+    unpurchased_courses_count
+  end
 end
