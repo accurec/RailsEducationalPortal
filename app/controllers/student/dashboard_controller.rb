8c73ab3c7f0d3cdf2293369778159df69fdb19c1
accurec <max.zhernovkov@gmail.com>
2025-08-11 05:23:40 +0000

    Optimize student dashboard controller to use precalculated stuff in the view

--- app/controllers/student/dashboard_controller.rb
+++ app/controllers/student/dashboard_controller.rb
@@ -4,14 +4,24 @@ class Student::DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    authorize :dashboard, :show_student?
-
     @student = User.includes(
       :school,
-      enrollments: {
-        course: { term: :school }
-      }
+      course_purchases: { course: :term },
+      enrollments: { course: :term }
     ).find(current_user.id)
+    
+    @ordered_course_purchases = @student.course_purchases.order(:purchased_at)
+    @ordered_enrollments = @student.enrollments.order(:enrolled_at)
+    
+    @course_purchases_count = @ordered_course_purchases.count
+    @enrollments_count = @ordered_enrollments.count
+    
+    @enrollment_status = @ordered_course_purchases.each_with_object({}) do |cp, hash|
+      hash[cp.course_id] = @ordered_enrollments.any? { |e| e.course_id == cp.course_id }
+    end
+    
+    @has_course_purchases = @course_purchases_count > 0
+    @has_enrollments = @enrollments_count > 0
   end
 
   def enroll_in_course
